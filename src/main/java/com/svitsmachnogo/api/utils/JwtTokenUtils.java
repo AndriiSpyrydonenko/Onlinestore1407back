96db847cc7b903a583689f40aaacbaa7b6df5ca4
Vanya Demydenko <educationvanyadem@gmail.com>
2023-10-21 23:11:15 +0300

    added a methods than getting claims from token

--- src/main/java/com/svitsmachnogo/api/utils/JwtTokenUtils.java
+++ src/main/java/com/svitsmachnogo/api/utils/JwtTokenUtils.java
@@ -1,7 +1,7 @@
 package com.svitsmachnogo.api.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.time.Duration;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,6 +43,22 @@ public class JwtTokenUtils {
                 .expiration(expiredDate)
                 .signWith(getSigningKey())
                 .compact();
+    }
+
+    public String getUserEmail(String token){
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public List<String> getRoles(String token){
+        return getAllClaimsFromToken(token).get("roles",List.class);
+    }
+
+    private Claims getAllClaimsFromToken(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     private SecretKey getSigningKey(){

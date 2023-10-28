0b71949a15690f5f05a4544a7051ecf044b651f8
Vanya Demydenko <educationvanyadem@gmail.com>
2023-10-28 14:41:50 +0300

    Added swagger documentation for each endpoint

--- src/main/java/com/svitsmachnogo/api/controller/AuthController.java
+++ src/main/java/com/svitsmachnogo/api/controller/AuthController.java
@@ -2,50 +2,63 @@ package com.svitsmachnogo.api.controller;
 
 import com.svitsmachnogo.api.dto.JwtRequestDTO;
 import com.svitsmachnogo.api.dto.JwtResponseDTO;
-import com.svitsmachnogo.api.exceptions.AuthError;
-import com.svitsmachnogo.api.service.UserService;
-import com.svitsmachnogo.api.utils.JwtTokenUtils;
-import org.springframework.http.HttpStatus;
+import com.svitsmachnogo.api.dto.RegistrationUserDTO;
+import com.svitsmachnogo.api.exceptions.AppError;
+import com.svitsmachnogo.api.exceptions.DifferentPasswordsExceptions;
+import com.svitsmachnogo.api.exceptions.UserAlreadyExistException;
+import com.svitsmachnogo.api.service.abstractional.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class AuthController {
 
-    private final UserService userService;
+    private final AuthService authService;
 
-    private final JwtTokenUtils jwtTokenUtils;
+    private final String authHeaderDescription = "This endpoint is used to authenticate a user and receive an " +
+            "authorization token (JWT) if the request is valid.The token must be included in the HTTP request " +
+            "header with the name 'Authorization', and it should be prefixed with 'Bearer ' followed by " +
+            "a space (e.g., 'Bearer your_JWT_token_here'). The token is considered valid for testing purposes " +
+            "for 1 minute after issuance.)";
 
-    private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserService userService, JwtTokenUtils jwtTokenUtils, AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.jwtTokenUtils = jwtTokenUtils;
-        this.authenticationManager = authenticationManager;
-    }
-
+    @Operation(summary = "Authenticate User and Get JWT Token", description = authHeaderDescription)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = JwtResponseDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Incorrect login or password!", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = AppError.class))})
+    })
     @PostMapping("/auth")
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequestDTO authRequest) {
-
-        try {
-            authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-        }catch (BadCredentialsException e){
-            return new ResponseEntity<>(new AuthError(HttpStatus.UNAUTHORIZED.value(), "Incorrect login or password!"),
-                    HttpStatus.UNAUTHORIZED);
-        }
-        UserDetails userDetails = userService.loadUserByUsername(authRequest.getEmail());
-        String token = jwtTokenUtils.generateToken(userDetails);
-
-        return ResponseEntity.ok(new JwtResponseDTO(token));
+        return authService.createJwtForUser(authRequest);
     }
 
+    @Operation(summary = "Registration", description = "The endpoint retrieve the user in the request body and returns a JWT that lives 1m or an AppError")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = JwtResponseDTO.class))}),
+            @ApiResponse(responseCode = "409", description = "Such user exist!", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = AppError.class))}),
+            @ApiResponse(responseCode = "400", description = "Confirm password is wrong!", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = AppError.class))})
+    })
+    @PostMapping("/registration")
+    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDTO registrationUserDTO) throws DifferentPasswordsExceptions, UserAlreadyExistException {
+        return authService.createNewUser(registrationUserDTO);
+    }
 }

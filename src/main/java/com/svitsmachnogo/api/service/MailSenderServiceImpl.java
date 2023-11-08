025033e317ddc63eeaad4ffa40d423657d19b8ee
Vanya Demydenko <educationvanyadem@gmail.com>
2023-11-08 23:51:26 +0200

    added javadoc

--- src/main/java/com/svitsmachnogo/api/service/MailSenderServiceImpl.java
+++ src/main/java/com/svitsmachnogo/api/service/MailSenderServiceImpl.java
@@ -6,18 +6,39 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+/**
+ * A service class that implements the MailSenderService interface for sending email messages.
+ */
 @Service
 @RequiredArgsConstructor
 public class MailSenderServiceImpl implements MailSenderService {
 
     private final JavaMailSender mailSender;
 
-    public void sendMail(String mailTo, String subject, String message){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("noreplay@gmail.com");
-        mailMessage.setTo(mailTo);
-        mailMessage.setText(message);
-
+    /**
+     * Sends an email using the provided SimpleMailMessage object.
+     *
+     * @param mailMessage The SimpleMailMessage object that contains information about the email.
+     * @author Vanya Demydenko
+     */
+    public void sendMail(SimpleMailMessage mailMessage) {
         mailSender.send(mailMessage);
+    }
+
+    /**
+     * Creates a SimpleMailMessage object with the specified recipient, subject, and message.
+     *
+     * @param mailTo  The recipient's email address.
+     * @param subject The subject of the email.
+     * @param message The body of the email.
+     * @return A SimpleMailMessage object ready for sending.
+     * @author Vanya Demydenko
+     */
+    public SimpleMailMessage createMessage(String mailTo, String subject, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mailTo);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+        return simpleMailMessage;
     }
 }

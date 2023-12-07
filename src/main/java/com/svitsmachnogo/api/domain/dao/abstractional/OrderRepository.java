3f6a577ce94c2fabfdab17a2ed76e4f7637bc558
Vanya Demydenko <educationvanyadem@gmail.com>
2023-12-07 23:53:49 +0200

    create OrderRepository

--- src/main/java/com/svitsmachnogo/api/domain/dao/abstractional/OrderRepository.java
+++ src/main/java/com/svitsmachnogo/api/domain/dao/abstractional/OrderRepository.java
@@ -7,12 +7,28 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository interface for managing Order entities.
+ */
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("from Order o where o.user.id = :id")
-    List<Order> findAllByUserId( Long id);
+    /**
+     * Finds all orders associated with a specific user ID.
+     *
+     * @param id The ID of the user.
+     * @return The list of orders associated with the given user ID.
+     */
+    @Query("from Order o where o.userProfile.id = :id")
+    List<Order> findAllByUserId(Long id);
 
+    /**
+     * Finds an order by its ID.
+     *
+     * @param id The ID of the order.
+     * @return An Optional containing the found Order entity, or empty if not found.
+     */
     @Override
     @Query("from Order o where o.id= :id")
     Optional<Order> findById(Long id);
 }
+

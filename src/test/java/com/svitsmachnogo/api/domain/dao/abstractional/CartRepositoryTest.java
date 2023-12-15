770075338b35751b89073e366f6d9be97735a0f5
Vanya Demydenko <educationvanyadem@gmail.com>
2023-12-15 18:40:12 +0200

    optimize  imports

--- src/test/java/com/svitsmachnogo/api/domain/dao/abstractional/CartRepositoryTest.java
+++ src/test/java/com/svitsmachnogo/api/domain/dao/abstractional/CartRepositoryTest.java
@@ -1,19 +1,12 @@
 package com.svitsmachnogo.api.domain.dao.abstractional;
 
 import com.svitsmachnogo.api.domain.entity.Cart;
-import com.svitsmachnogo.api.domain.entity.Packaging;
-import com.svitsmachnogo.api.domain.entity.PackagingId;
-import com.svitsmachnogo.api.service.packaging.PackagingService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest

aedc6cefa987313e279721ea08b0deadfb528899
VanyaDem <educationvanyadem@gmail.com>
2024-01-24 22:38:48 +0200

    added OpenApi documentation

--- src/main/java/com/svitsmachnogo/api/controller/UploadFileController.java
+++ src/main/java/com/svitsmachnogo/api/controller/UploadFileController.java
@@ -2,6 +2,7 @@ package com.svitsmachnogo.api.controller;
 
 import com.svitsmachnogo.api.service.file.GCPFileService;
 import com.svitsmachnogo.api.service.file.upload.UploadType;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,16 +18,17 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping("/api/upload") // todo: eventually the correct route will be added as "/api/admin/files"
+@RequestMapping("/api/admin/files") // todo: eventually the correct route will be added as "/api/admin/files"
 @Tag(name = "Upload files controller")
 public class UploadFileController {
 
     private final GCPFileService fileService;
 
-    @PostMapping(
-            value = "/products",
-            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<List<String>> addPicturesForProduct(@RequestParam("files")MultipartFile[] files){
+
+    @PostMapping(value = "/products", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "Upload pictures for product"
+            , description = "Admin only. Picture size must be less than 10mb.The data type must be .jpg .jpeg or .png")
+    public ResponseEntity<List<String>> addPicturesForProduct(@RequestParam("files") MultipartFile[] files) {
         List<String> res = Arrays.stream(files)
                 .map(f -> fileService.uploadFile(f, UploadType.PRODUCT))
                 .collect(Collectors.toList());
@@ -37,12 +38,12 @@ public class UploadFileController {
     @PostMapping(
             value = "/categories",
             consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<Map<String,String>> addPicturesForCategories(@RequestParam("hover")MultipartFile hover,
-                                                                @RequestParam("fill")MultipartFile fill){
-        Map<String,String> pictures = new HashMap<>();
-        pictures.put("hover", fileService.uploadFile(hover,UploadType.CATEGORY));
-        pictures.put("fill", fileService.uploadFile(fill,UploadType.CATEGORY));
-
-        return ResponseEntity.ok(pictures);
+    @Operation(summary = "Upload pictures for category"
+            , description = "Admin only. Picture size must be less than 10mb.The data type must be .svg")
+    public ResponseEntity<Map<String, String>> addPicturesForCategories(@RequestParam("hover") MultipartFile hover,
+                                                                        @RequestParam("fill") MultipartFile fill) {
+        return ResponseEntity.ok(
+                Map.of("hover", fileService.uploadFile(hover, UploadType.CATEGORY),
+                        "fill", fileService.uploadFile(fill, UploadType.CATEGORY)));
     }
 }

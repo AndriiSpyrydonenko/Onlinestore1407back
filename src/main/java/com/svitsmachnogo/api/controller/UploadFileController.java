8e2cce6dc2927d2bf249c7a21437dc48c34ab71d
VanyaDem <educationvanyadem@gmail.com>
2024-01-24 16:57:37 +0200

    added endpoints for uploading pictures
    * for category
    * for products

--- src/main/java/com/svitsmachnogo/api/controller/UploadFileController.java
+++ src/main/java/com/svitsmachnogo/api/controller/UploadFileController.java
@@ -1,6 +1,7 @@
 package com.svitsmachnogo.api.controller;
 
 import com.svitsmachnogo.api.service.file.GCPFileService;
+import com.svitsmachnogo.api.service.file.upload.UploadType;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -9,23 +10,39 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping("/api/upload")
+@RequestMapping("/api/upload") // todo: eventually the correct route will be added as "/api/admin/files"
 @Tag(name = "Upload files controller")
 public class UploadFileController {
 
     private final GCPFileService fileService;
 
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<List<String>> addFile(@RequestParam("files")MultipartFile[] files){
+    @PostMapping(
+            value = "/products",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<List<String>> addPicturesForProduct(@RequestParam("files")MultipartFile[] files){
         List<String> res = Arrays.stream(files)
-                .map(fileService::uploadFile)
+                .map(f -> fileService.uploadFile(f, UploadType.PRODUCT))
                 .collect(Collectors.toList());
         return ResponseEntity.ok(res);
+    }
+
+    @PostMapping(
+            value = "/categories",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Map<String,String>> addPicturesForCategories(@RequestParam("hover")MultipartFile hover,
+                                                                @RequestParam("fill")MultipartFile fill){
+        Map<String,String> pictures = new HashMap<>();
+        pictures.put("hover", fileService.uploadFile(hover,UploadType.CATEGORY));
+        pictures.put("fill", fileService.uploadFile(fill,UploadType.CATEGORY));
+
+        return ResponseEntity.ok(pictures);
     }
 }

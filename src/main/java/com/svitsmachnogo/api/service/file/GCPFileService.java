3742737c1d42c598971ea498f89805a5ae1c8efa
VanyaDem <educationvanyadem@gmail.com>
2024-01-24 15:22:44 +0200

    mwp for uploading files to GCP storage

--- src/main/java/com/svitsmachnogo/api/service/file/GCPFileService.java
+++ src/main/java/com/svitsmachnogo/api/service/file/GCPFileService.java
@@ -6,22 +6,19 @@ import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
 @Service
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class GCPFileService {
 
-    @Autowired
-    private Storage storage;
+    private final Storage storage;
 
     @Value("${gcp.bucket.name}")
     private String gcpBucketName;
@@ -32,33 +29,22 @@ public class GCPFileService {
     @Value("${gcp.dir.product.name}")
     private String gcpDirProduct;
 
+    @Value("${gcp.cloud.storage.link}")
+    private String gcpStorageLink;
+
     public String uploadFile(MultipartFile file) {
            try {
-               BlobId blobId = BlobId.of(gcpBucketName, file.getOriginalFilename());
-               BlobInfo blobInfo = BlobInfo.newBuilder(blobId).
-                       setContentType(file.getContentType()).build();
+
+               BlobId blobId = BlobId.of(gcpBucketName, gcpDirCategory + file.getOriginalFilename());
+
+               BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(file.getContentType()).build();
 
                Blob blob = storage.create(blobInfo,file.getBytes());
 
-               return blob.getMediaLink();
+               return gcpStorageLink + gcpDirCategory + blob.getName();
 
            }catch(Exception e){
                throw new RuntimeException(e);
            }
-    }
-
-    private File convertToFile(MultipartFile multipartFile) {
-        try {
-            if (multipartFile.getOriginalFilename() == null) {
-                throw new RuntimeException("Original file name is null !!!");
-            }
-            File convertedFile = new File(multipartFile.getName());
-            FileOutputStream outputStream = new FileOutputStream(convertedFile);
-            outputStream.write(multipartFile.getBytes());
-            outputStream.close();
-            return convertedFile;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }

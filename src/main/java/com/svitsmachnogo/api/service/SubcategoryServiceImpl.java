8d6433165d4f2d0245dc9531b172bfea84aefd64
VanyaDem <educationvanyadem@gmail.com>
2024-01-27 19:18:49 +0200

    added save method

--- src/main/java/com/svitsmachnogo/api/service/SubcategoryServiceImpl.java
+++ src/main/java/com/svitsmachnogo/api/service/SubcategoryServiceImpl.java
@@ -7,7 +7,6 @@ import com.svitsmachnogo.api.domain.entity.Subcategory;
 import com.svitsmachnogo.api.service.abstractional.SubcategoryService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +25,10 @@ public class SubcategoryServiceImpl implements SubcategoryService {
     @Override
     @Transactional
     public Set<Product> getProductsBySubcategory(String subcategoryId) {
-        return subcategoryDAO.findById(subcategoryId).get().getProducts();
+        return subcategoryDAO
+                .findById(subcategoryId)
+                .orElseThrow()
+                .getProducts();
     }
 
     @Override
@@ -45,5 +47,11 @@ public class SubcategoryServiceImpl implements SubcategoryService {
 
     private static void setCountProductsForSubcategory(List<Subcategory> subcategories) {
         subcategories.forEach(s -> s.setProductCount(s.getProducts().size()));
+    }
+
+    @Override
+    @Transactional
+    public void save(Subcategory subcategory) {
+        subcategoryRepository.save(subcategory);
     }
 }

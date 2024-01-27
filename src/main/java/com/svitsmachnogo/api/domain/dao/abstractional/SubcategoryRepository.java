df548b79e3f508b2f5e80a8258846068cebbba70
VanyaDem <educationvanyadem@gmail.com>
2024-01-27 15:45:35 +0200

    removed custom query and added @Fetch annotation

--- src/main/java/com/svitsmachnogo/api/domain/dao/abstractional/SubcategoryRepository.java
+++ src/main/java/com/svitsmachnogo/api/domain/dao/abstractional/SubcategoryRepository.java
@@ -1,8 +1,9 @@
 package com.svitsmachnogo.api.domain.dao.abstractional;
 
 import com.svitsmachnogo.api.domain.entity.Subcategory;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,10 +12,6 @@ import java.util.Optional;
 public interface SubcategoryRepository extends JpaRepository<Subcategory, String> {
 
     @Override
-    @Query("""
-            from Subcategory s 
-            join fetch s.products 
-            where s.id = :id
-            """)
-    Optional<Subcategory> findById(String id);
+    @Fetch(value = FetchMode.JOIN)
+    Optional<Subcategory> findById(String subcategoryId);
 }

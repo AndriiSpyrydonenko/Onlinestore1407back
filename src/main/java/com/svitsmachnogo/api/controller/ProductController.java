8d3c61ae310de1f49e0fface8166452a422ded89
VanyaDem <educationvanyadem@gmail.com>
2024-01-28 13:06:56 +0200

    added OpenApi documentation for endpoints

--- src/main/java/com/svitsmachnogo/api/controller/ProductController.java
+++ src/main/java/com/svitsmachnogo/api/controller/ProductController.java
@@ -1,13 +1,13 @@
 package com.svitsmachnogo.api.controller;
 
 import com.svitsmachnogo.api.domain.entity.Subcategory;
-import com.svitsmachnogo.api.dto.product.AddProductDto;
-import com.svitsmachnogo.api.dto.subcategory.SubcategoryDTO;
+import com.svitsmachnogo.api.dto.product.ProductAdditionDto;
 import com.svitsmachnogo.api.dto.subcategory.SubcategorySimpleDto;
 import com.svitsmachnogo.api.dto.subcategory.SubcategorySimpleDtoFactory;
 import com.svitsmachnogo.api.service.abstractional.SubcategoryService;
 import com.svitsmachnogo.api.service.product.ManageProductService;
 import com.svitsmachnogo.api.utils.DtoUtils;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,26 +21,30 @@ import java.util.List;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @Tag(name = "Product Controller", description = "Endpoints for managing products. This endpoint is only accessible to an administrator.")
-@RequestMapping("/api/test/products")
+@RequestMapping("/api/admin/products")
 public class ProductController {
 
     private final ManageProductService productService;
 
     private final SubcategoryService subcategoryService;
 
+    @Operation(summary = "Add product.", description = "Is protected! Admin only.   ... ")
+    //todo: after the merge to add into description the picture addition process and ^ reference for it.
     @PutMapping()
-    public ResponseEntity<?> addProduct(@RequestBody AddProductDto product){
+    public ResponseEntity<?> addProduct(@RequestBody ProductAdditionDto product) {
         productService.addProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Get subcategory.",
+                description = "Is protected! Admin only.Receives the categoryId as key of request parameter, value is a specific category ID (e.g 'nuts'). Returns the SUBCATEGORY list for a specific CATEGORY.")
     @GetMapping("/products/subcategories")
     public ResponseEntity<List<SubcategorySimpleDto>> getSubcategoriesByCategoryId(
             @RequestParam(name = "categoryId")
-            @Parameter(name = "categoryId" ,required = true ,description = "The category id of target product")
-            String categoryId){
+            @Parameter(name = "categoryId", required = true, description = "The category id of target product")
+            String categoryId) {
 
         List<Subcategory> subcategories = subcategoryService.getAllSubcategoryByCategoryId(categoryId);
-        return ResponseEntity.ok(DtoUtils.listOf(subcategories,new SubcategorySimpleDtoFactory()));
+        return ResponseEntity.ok(DtoUtils.listOf(subcategories, new SubcategorySimpleDtoFactory()));
     }
 }
